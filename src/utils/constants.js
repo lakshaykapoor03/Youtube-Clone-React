59863611d7e0caab8bc097f6b8ba418e11675351
lakshaykapoor03 <kapoorlakshay70@gmail.com>
2023-06-23 12:07:08 +0530

    changed http to https in suggestions api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const YOUTUBE_API= "https://youtube.googleapis.com/youtube/v3/videos?part
 
 export const VIDEO_DATA_API= "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc%2Cc0KYU2j0TM4%2CeIho2S0ZahI&key="+ API_KEY;
 
-export const SUGGESTIONS_API= "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&ds=yt&q="
+export const SUGGESTIONS_API= "https://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&ds=yt&q="
 
 export const COMMENTS_API= " https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId="
 
